c2789b6cf5661eb138684d87e68a95b47a8234ba
Paula Herrera <herrera.paula5@gmail.com>
2021-02-03 17:05:27 -0700

    Created seeding document for attractions

--- db/seeding/seed_attractions.js
+++ db/seeding/seed_attractions.js
@@ -1,11 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable func-names */
 const fs = require('fs');
 const faker = require('faker');
-const argv = require('yargs').argv
+const { argv } = require('yargs');
 
-const lines = argv.lines || 10;
-const filename = argv.output || 'posts.csv';
-const writeStream = fs.createWriteStream(filename);
+const lines = argv.lines || 10000000;
+const filename = argv.output || 'attractions.csv';
+const stream = fs.createWriteStream(filename);
 
 const attractionTypes = [
   'National Park',
@@ -27,18 +28,14 @@ const descriptions = [
   'Furl brig Arr mutiny keel spyglass pressgang reef deadlights lateen sail hands Jack Ketch line barque gunwalls lad nipper parley overhaul bring a spring upon her cable.',
   'Shrouds boatswain main sheet run a rig weigh anchor black spot league broadside skysail draft transom ballast American Main port',
   'Jolly Roger me bring a spring upon her cable dead men tell no tales plunder to go on account',
-  'Come on up and see me urchins. Wanna shiver me timbers? So, tell me, why do they call ye, “Cap’n Feathersword?”',
-  'Even pirates, before they attack another ship, hoist a black flag. The Code is more like guidelines, really.',
-  'Yes, that is a hornpipe in my pocket and I am happy to see you. They don’t call me Long John because my head is so big.',
+  'Come on up and see me urchins. Wanna shiver me timbers? So tell me why do they call ye, “Cap’n Feathersword?”',
+  'Even pirates before they attack another ship hoist a black flag. The Code is more like guidelines, really.',
+  'Yes that is a hornpipe in my pocket and I am happy to see you. They don’t call me Long John because my head is so big.',
   'You can always trust the untrustworthy because you can always trust that they will be untrustworthy.',
   'Its the trustworthy you can’t trust. Not all treasure is silver and gold Where there is a sea there are pirates.',
-  'Damnation seize my soul if I give you quarters, or take any from you. It’s not everyday you get to do a pirate movie, you might as well go for it.',
+  'Damnation seize my soul if I give you quarters or take any from you. It’s not everyday you get to do a pirate movie you might as well go for it.',
   'How much does the pirate pay for an ear piercing?',
 ];
-
-const urlGen = function (id) {
-  return `https://awsm-hr.s3.us-east-2.amazonaws.com/showcaseImages/images/image${id}.jpg`;
-};
 
 const randomGenerator = function (min, list) {
   if (typeof list === 'number') {
@@ -47,34 +44,51 @@ const randomGenerator = function (min, list) {
   return list[Math.floor(Math.random() * list.length)];
 };
 
-const picsListGen = function () {
-  const pics = [];
-  const max = Math.floor(Math.random() * 30) + 1;
-  for (let i = 0; i < max; i += 1) {
-    pics.push(urlGen(randomGenerator(0, 100)));
-  }
-  return pics;
-};
-
 const bools = [true, false];
 
-const createAttraction = (id) => {
+const createAttraction = () => {
   const location = faker.address.city();
-  const attractionTitle = faker.commerce.productName;
+
+  const attractionTitle = faker.commerce.productName();
   const city = location;
   const reviews = randomGenerator(0, 3000);
-  const relativeRanking = [randomGenerator(1, 34), randomGenerator(35, 101)];
-  const ratio = relativeRanking[0] / relativeRanking[1];
+  const relativeRanking1 = randomGenerator(1, 34);
+  const relativeRanking2 = randomGenerator(35, 101);
+  const ratio = relativeRanking1 / relativeRanking2;
   const attractionType = randomGenerator(null, attractionTypes);
   const description = randomGenerator(null, descriptions);
   const isOpen = randomGenerator(null, bools);
   const suggestedDuration = randomGenerator(0, 200);
-  const address = `${faker.address.streetAddress()}, ${location}, ${faker.address.zipCode}, ${faker.address.country}`;
-  const imageUrl = [urlGen(id), ...picsListGen()];
+  const address = `${faker.address.streetAddress()} ${location} ${faker.address.zipCode()} ${faker.address.country()}`;
   const travelersChoiceAward = randomGenerator(null, bools);
   const likedStatus = randomGenerator(null, bools);
   const ticketPrice = randomGenerator(0, 500);
   const averageRating = randomGenerator(0, 25) / 5;
 
-  return `${attractionTitle},${city},${reviews},${relativeRanking},${ratio},${attractionType},${description},${isOpen},${suggestedDuration},${address},${imageUrl},${travelersChoiceAward},${likedStatus},${ticketPrice},${averageRating}\n`
+  return `${attractionTitle},${city},${reviews},${relativeRanking1},${relativeRanking2},${ratio},${attractionType},${description},${isOpen},${suggestedDuration},${address},${travelersChoiceAward},${likedStatus},${ticketPrice},${averageRating}\n`;
 };
+
+const startWriting = (writeStream, encoding, done) => {
+  let i = lines;
+  function writing() {
+    const canWrite = true;
+    do {
+      i -= 1;
+      const post = createAttraction();
+      if (i === 0) {
+        writeStream.write(post, encoding, done);
+      } else {
+        writeStream.write(post, encoding);
+      }
+    } while (i > 0 && canWrite);
+    if (i > 0 && !canWrite) {
+      writeStream.once('drain', writing);
+    }
+  }
+  writing();
+};
+
+stream.write('attractionTitle,city,reviews,relativeRanking1,relativeRanking2,ratio,attractionType,description,isOpen,suggestedDuration,address,travelersChoiceAward,linkedStatus,ticketPrice,averageRating\n', 'utf-8');
+startWriting(stream, 'utf-8', () => {
+  stream.end();
+});
--- unsplash-scraper/scraper.js
+++ unsplash-scraper/scraper.js
@@ -18,10 +18,10 @@ const searchAndSaveUnsplash = function (searchTerm, qty, page, relativePath) {
   axios.get(unsplashURL)
     .then(({ data }) => {
       data.results.forEach((result, i) => {
-        saveImageToDisk(result.urls.regular, `${relativePath}${i + 998}.jpg`);
+        saveImageToDisk(result.urls.regular, `${relativePath}${i}.jpg`);
       });
     })
     .catch((err) => console.log(err));
 };
 
-searchAndSaveUnsplash('nepal', 2, 2, '../images/image');
+searchAndSaveUnsplash('denver', 10, 2, '../test/image');
