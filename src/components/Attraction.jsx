62416de49da71644bc6e3974fb35cb1fe6911eb0
Paula Herrera <herrera.paula5@gmail.com>
2021-02-09 20:07:48 -0700

    Changed localhost to new IP

--- src/components/Attraction.jsx
+++ src/components/Attraction.jsx
@@ -28,7 +28,7 @@ export default class Attraction extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:3001/api/showcase')
+    axios.get('http://52.14.8.88:3001/api/showcase')
       .then(({ data }) => {
         this.setState({
           current: data[0],
